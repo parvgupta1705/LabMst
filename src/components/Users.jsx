b993c790d9de6e676e269d0d917ed8c455e6be1b
Parv Gupta <23bai70205@cuchd.in>
2026-03-10 11:52:16 +0530

    Lab MST

---
+++ src/components/Users.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
